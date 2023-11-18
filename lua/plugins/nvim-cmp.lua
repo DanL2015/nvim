878c102cb8978e5868f15a88de56d34a9d72ba97
Daniel Liu <daniel17liu@gmail.com>
2023-11-17 19:03:43 -0800

    Added tex snippets for luasnip

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -27,8 +27,8 @@ return {
         end,
       },
       mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
         ["<C-Space>"] = cmp.mapping.complete(),     -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(),            -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,5 +2,7 @@ return {
   "lervag/vimtex",
   init = function()
     vim.g.vimtex_view_method = "zathura"
+    vim.g.tex_conceal='abdmgs'
+    vim.o.conceallevel = 1
   end,
 }
