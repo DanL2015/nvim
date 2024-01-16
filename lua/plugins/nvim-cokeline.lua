f59897f041dd2d63a05ee63825950695b953d456
Daniel Liu <daniel17liu@gmail.com>
2024-01-15 18:44:23 -0800

    better cokeline, error/warning colors and font compatability with cozette

--- lua/plugins/nvim-cokeline.lua
+++ lua/plugins/nvim-cokeline.lua
@@ -8,6 +8,58 @@ return {
   config = function()
     local get_hex = require('cokeline.hlgroups').get_hl_attr
     require("cokeline").setup({
+      default_hl = {
+        fg = function(buffer)
+          return
+              buffer.is_focused
+              and get_hex('ColorColumn', 'bg')
+              or get_hex('Normal', 'fg')
+        end,
+        bg = function(buffer)
+          return
+              buffer.is_focused
+              and get_hex('Normal', 'fg')
+              or get_hex('ColorColumn', 'bg')
+        end,
+      },
+
+      components = {
+        {
+          text = function(buffer) return (buffer.index ~= 1) and '▏' or '' end,
+          fg = function() return get_hex('Normal', 'fg') end
+        },
+        {
+          text = function(buffer) return '    ' .. buffer.devicon.icon end,
+          fg = function(buffer) return buffer.devicon.color end,
+        },
+        {
+          text = function(buffer) return buffer.unique_prefix end,
+          fg = get_hex('Comment', 'fg'),
+          italic = true
+        },
+        {
+          text = function(buffer) return buffer.filename .. '    ' end,
+          fg = function(buffer)
+            return
+                (buffer.diagnostics.errors ~= 0 and get_hex('DiagnosticError', 'fg'))
+                or (buffer.diagnostics.warnings ~= 0 and get_hex('DiagnosticWarn', 'fg'))
+                or nil
+          end,
+          underline = function(buffer)
+            return buffer.is_hovered and not buffer.is_focused
+          end
+        },
+        {
+          text = function(buffer) return buffer.is_modified and '●' or '✕' end,
+          fg = function(buffer) return buffer.is_modified and vim.g.terminal_color_2 or nil end,
+          on_click = function(_, _, _, _, buffer)
+            buffer:delete()
+          end
+        },
+        {
+          text = ' ',
+        }
+      },
       sidebar = {
         filetype = { "NvimTree" },
         components = {
